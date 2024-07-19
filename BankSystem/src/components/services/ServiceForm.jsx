d782cd7a2b1d743f920d941bd14a61daed619ed0
jboteo-2019465 <jboteo-2019465@kinal.edu.gt>
2024-07-18 21:12:16 -0600

    cambios

--- BankSystem/src/components/services/ServiceForm.jsx
+++ BankSystem/src/components/services/ServiceForm.jsx
@@ -8,7 +8,7 @@ export const ServiceForm = () => {
         name: '',
         description: '',
         price: '',
-        img: null 
+        img: null   
     });
 
     const handleChange = (e) => {
